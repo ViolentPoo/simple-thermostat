7455597a3b75ecdefc8fceaed3f943a7446771df
Wheemer <wheemer@hotmail.com>
2026-05-09 09:07:47 -0230

    Update infoItem.ts
    fix: preserve entity heading render spacing

--- src/components/infoItem.ts
+++ src/components/infoItem.ts
@@ -127,8 +127,8 @@ export default function renderInfoItem({
   const tooltip = heading || state?.attributes?.friendly_name || state?.entity_id
 
   const headingResult = icon
-    ? html`<ha-icon icon="${icon}" title=${tooltip}></ha-icon>`
-    : html`${heading}:`
+    ? html` <ha-icon icon="${icon}" title=${tooltip}></ha-icon> `
+    : html` ${heading}: `
 
   const entityId = typeof state === 'object' ? state.entity_id : null
 
