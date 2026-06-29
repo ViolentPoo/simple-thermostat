3505dbe5cc01409f6b4fd47e98068bc51983e6b2
Tim <48303844+ViolentPoo@users.noreply.github.com>
2026-06-29 13:27:07 -0400

    Refactor getEntityType to check HVAC mode
    Signed-off-by: Tim <48303844+ViolentPoo@users.noreply.github.com>

--- src/getEntityType.ts
+++ src/getEntityType.ts
@@ -2,11 +2,13 @@ export const DUAL = 'dual'
 export const SINGLE = 'single'
 
 export default function getEntityType(attributes) {
-  if (
-    typeof attributes.target_temp_high === 'number' &&
-    typeof attributes.target_temp_low === 'number'
-  ) {
+  const mode = attributes?.hvac_mode
+
+  const isDualMode = mode === 'heat_cool' || mode === 'auto'
+
+  if (isDualMode) {
     return DUAL
   }
+
   return SINGLE
 }
